05efc5e1521f50e31db77b39b6e14c0b3e4d954f
Rogit100 <Rohitsaini1920ok@gmail.com>
2024-07-22 11:10:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7396052535:AAGxUs1Yq8vDs-UlShqHkaxUuspGlEiPNT8")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("KrishnaXcrazy")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("7028596834")
 
 
 
